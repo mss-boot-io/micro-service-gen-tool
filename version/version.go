d7144b16f6147c50ec1f0ac5fdfcbcfa7422572a
lwnmengjing <991154416@qq.com>
2022-03-10 22:06:03 +0800

    :memo: update version.go

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 import "fmt"
 
-const Version = "v0.1.2"
+const Version = "v0.1.3"
 
 // getVersion Compulsory minimum version, Minimum downward compatibility to this version
 func getVersion() string {
